81eaa7098e87a146f76748bbb04a53a7ca9012ce
ARUN5gSRM <148932572+ARUN5gSRM@users.noreply.github.com>
2025-11-07 09:15:21 +0530

    Delete chatbot_project directory

--- chatbot_project/cb_app/static/js/main.js
+++
@@ -1,8 +0,0 @@
-
-// Auto-scroll chatbox to bottom
-document.addEventListener("DOMContentLoaded", function() {
-    const chatbox = document.getElementById("chatbox");
-    if (chatbox) {
-        chatbox.scrollTop = chatbox.scrollHeight;
-    }
-});
